0d1ef774f0ec97a24e7f8b680b64a7c762533fbd
Kyle Roach <kroach.work@gmail.com>
2017-09-03 10:54:57 -0400

    feat(tooling): Add typescript definitions (#7)

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+// Type definitions for react-native-store-review 0.1
+// Project: https://github.com/oblador/react-native-store-review
+// Definitions by: Kyle Roach <https://github.com/iRoachie>
+// TypeScript Version: 2.4
+
+export const isAvailable: boolean
+export function requestReview(): void
