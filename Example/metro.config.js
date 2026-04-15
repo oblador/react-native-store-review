a07a41fd949a8b7f8e73a81ffa8f48ed389266fe
Joel Arvidsson <joel@oblador.se>
2026-04-15 11:29:04 +0200

    Update React Native to 0.85

--- Example/metro.config.js
+++ Example/metro.config.js
@@ -2,7 +2,9 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 /**
  * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
+ * https://reactnative.dev/docs/metro
+ *
+ * @type {import('@react-native/metro-config').MetroConfig}
  */
 const config = {};
 
